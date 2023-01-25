b20f5aa140d74d3af4c689e18d46dc383eb6d2cd
Elviiss <elvis22elmejor@gmail.com>
2023-01-25 13:44:12 +0100

    cambios

---
+++ routes/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const pool = require('../db')
+
+/* GET home page. */
+router.get('/', async function(req, res, next) {
+  const [rows] = await pool.query('SELECT * FROM users')
+  res.json(rows)
+});
+
+router.get('/ping', async function(req, res, next) {
+  const result = await pool.query('SELECT "hello world" as RESULT')
+  console.log(result)
+  res.json(result[0]);
+});
+
+router.get('/create', async function(req, res, next) {
+  const result = await pool.query('INSERT INTO users(name) VALUES ("Canelo")')
+  res.json(result);
+});
+
+module.exports = router;
